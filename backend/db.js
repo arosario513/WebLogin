f0e90d8e7a7581d76dd01a7924e312c1179914b9
arosario513 <a.rosario51304@gmail.com>
2024-11-02 11:09:00 -0400

    formatting

--- backend/db.js
+++ backend/db.js
@@ -1,16 +1,19 @@
 process.loadEnvFile("../.env");
+
 const { createConnection } = require("mysql2");
+
 const conn = createConnection({
-    host: "localhost",
-    user: "root",
-    password: process.env.MYSQL_ROOT_PASSWORD,
-    database: "userdb",
+  host: "localhost",
+  user: "root",
+  password: process.env.MYSQL_ROOT_PASSWORD,
+  database: "userdb",
 });
+
 conn.connect((err) => {
-    if (err) {
-        console.error("Could not connect to database", err.stack);
-    }
-    console.log("Connected to database as ", conn.threadId);
+  if (err) {
+    console.error("Could not connect to database", err.stack);
+  }
+  console.log("Connected to database as ", conn.threadId);
 });
 
 module.exports = conn;
